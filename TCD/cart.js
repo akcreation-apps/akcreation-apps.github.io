417dbc0bf6a0cd8e9def77113cde3235cb1c66be
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2024-10-16 10:32:10 +0530

    Backup_wp_no changed

--- TCD/cart.js
+++ TCD/cart.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js';
 import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc, doc, Timestamp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js';
-const BACKUP_WP_NO = "+917749984274"
+const BACKUP_WP_NO = "+918920042482"
 // Retrieve the cart from localStorage
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
