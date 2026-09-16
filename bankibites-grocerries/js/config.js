b6d5bf17592bdb89ed927eb77472e7a89d849e9b
anil_kr_sahoo <anil.sahoo@aeccglobal.io>
2026-09-16 21:01:36 +0530

    Check disable grocery

--- bankibites-grocerries/js/config.js
+++ bankibites-grocerries/js/config.js
@@ -98,7 +98,7 @@ window.BB_CONFIG = {
   // toggle here disables the whole storefront.
   // -----------------------------------------------------------------
   maintenance: {
-    enabled: false,
+    enabled: true,
     title: 'Restocking the shelves',
     message:
       "BankiBites is polishing the counters and refilling the aisles. We'll be back shortly with fresher shelves and a smoother checkout.",
